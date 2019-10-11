dc4cb84df0f623eb3c0c70d4a056813581c9b629
ihgazni2 <terryinzaghi@163.com>
2019-10-11 08:40:31 +0000

    remove old build

--- ematmap/filter_engine.py
+++ ematmap/filter_engine.py
@@ -121,7 +121,37 @@ def durl_wrap(func):
     return(wrapper)
 
 
+def ffltre(d):
+    m = d['m']
+    cond_func = d['f']
+    other_args = d['o']
+    x = d['x']
+    y = d['y']
+    v = m[x][y]
+    ele = cond_func(*other_args)
+    return(ele)
 
+def xfltre(d):
+    m = d['m']
+    cond_func = d['f']
+    other_args = d['o']
+    x = d['x']
+    y = d['y']
+    v = m[x][y]
+    ele = cond_func(x,*other_args)
+    return(ele)
+
+
+
+def yfltre(d):
+    m = d['m']
+    cond_func = d['f']
+    other_args = d['o']
+    x = d['x']
+    y = d['y']
+    v = m[x][y]
+    ele = cond_func(y,*other_args)
+    return(ele)
 
 
 def vfltre(d):
@@ -135,33 +165,307 @@ def vfltre(d):
     return(ele)
 
 
+def ofltre(d):
+    m = d['m']
+    cond_func = d['f']
+    other_args = d['o']
+    x = d['x']
+    y = d['y']
+    v = m[x][y]
+    ele = cond_func(*other_args)
+    return(ele)
+
+
+
+def fxfltre(d):
+    m = d['m']
+    cond_func = d['f']
+    other_args = d['o']
+    x = d['x']
+    y = d['y']
+    v = m[x][y]
+    ele = cond_func(x,*other_args)
+    return(ele)
 
 
 
 
+def fyfltre(d):
+    m = d['m']
+    cond_func = d['f']
+    other_args = d['o']
+    x = d['x']
+    y = d['y']
+    v = m[x][y]
+    ele = cond_func(y,*other_args)
+    return(ele)
 
 
 
+def fvfltre(d):
+    m = d['m']
+    cond_func = d['f']
+    other_args = d['o']
+    x = d['x']
+    y = d['y']
+    v = m[x][y]
+    ele = cond_func(v,*other_args)
+    return(ele)
 
 
 
+def fofltre(d):
+    m = d['m']
+    cond_func = d['f']
+    other_args = d['o']
+    x = d['x']
+    y = d['y']
+    v = m[x][y]
+    ele = cond_func(o,*other_args)
+    return(ele)
+
+
+def xyfltre(d):
+    m = d['m']
+    cond_func = d['f']
+    other_args = d['o']
+    x = d['x']
+    y = d['y']
+    v = m[x][y]
+    ele = cond_func(x,y,*other_args)
+    return(ele)
 
 
 
+def xvfltre(d):
+    m = d['m']
+    cond_func = d['f']
+    other_args = d['o']
+    x = d['x']
+    y = d['y']
+    v = m[x][y]
+    ele = cond_func(x,v,*other_args)
+    return(ele)
+
+
+def xofltre(d):
+    m = d['m']
+    cond_func = d['f']
+    other_args = d['o']
+    x = d['x']
+    y = d['y']
+    v = m[x][y]
+    ele = cond_func(x,*other_args)
+    return(ele)
+
+
+def yvfltre(d):
+    m = d['m']
+    cond_func = d['f']
+    other_args = d['o']
+    x = d['x']
+    y = d['y']
+    v = m[x][y]
+    ele = cond_func(y,v,*other_args)
+    return(ele)
+
+
+def yofltre(d):
+    m = d['m']
+    cond_func = d['f']
+    other_args = d['o']
+    x = d['x']
+    y = d['y']
+    v = m[x][y]
+    ele = cond_func(y,*other_args)
+    return(ele)
+
+
+def vofltre(d):
+    m = d['m']
+    cond_func = d['f']
+    other_args = d['o']
+    x = d['x']
+    y = d['y']
+    v = m[x][y]
+    ele = cond_func(v,*other_args)
+    return(ele)
+
+
+def fxyfltre(d):
+    m = d['m']
+    cond_func = d['f']
+    other_args = d['o']
+    x = d['x']
+    y = d['y']
+    v = m[x][y]
+    ele = cond_func(x,y,*other_args)
+    return(ele)
+
+
+def fxvfltre(d):
+    m = d['m']
+    cond_func = d['f']
+    other_args = d['o']
+    x = d['x']
+    y = d['y']
+    v = m[x][y]
+    ele = cond_func(x,v,*other_args)
+    return(ele)
+
+
+def fxofltre(d):
+    m = d['m']
+    cond_func = d['f']
+    other_args = d['o']
+    x = d['x']
+    y = d['y']
+    v = m[x][y]
+    ele = cond_func(x,o,*other_args)
+    return(ele)
+
+
+def fyvfltre(d):
+    m = d['m']
+    cond_func = d['f']
+    other_args = d['o']
+    x = d['x']
+    y = d['y']
+    v = m[x][y]
+    ele = cond_func(y,v,*other_args)
+    return(ele)
+
+
+def fyofltre(d):
+    m = d['m']
+    cond_func = d['f']
+    other_args = d['o']
+    x = d['x']
+    y = d['y']
+    v = m[x][y]
+    ele = cond_func(y,o,*other_args)
+    return(ele)
+
+
+def fvofltre(d):
+    m = d['m']
+    cond_func = d['f']
+    other_args = d['o']
+    x = d['x']
+    y = d['y']
+    v = m[x][y]
+    ele = cond_func(v,o,*other_args)
+    return(ele)
+
+
+def xyvfltre(d):
+    m = d['m']
+    cond_func = d['f']
+    other_args = d['o']
+    x = d['x']
+    y = d['y']
+    v = m[x][y]
+    ele = cond_func(x,y,v,*other_args)
+    return(ele)
+
+
+def xyofltre(d):
+    m = d['m']
+    cond_func = d['f']
+    other_args = d['o']
+    x = d['x']
+    y = d['y']
+    v = m[x][y]
+    ele = cond_func(x,y,*other_args)
+    return(ele)
+
+
+def xvofltre(d):
+    m = d['m']
+    cond_func = d['f']
+    other_args = d['o']
+    x = d['x']
+    y = d['y']
+    v = m[x][y]
+    ele = cond_func(x,v,*other_args)
+    return(ele)
 
 
 
+def yvofltre(d):
+    m = d['m']
+    cond_func = d['f']
+    other_args = d['o']
+    x = d['x']
+    y = d['y']
+    v = m[x][y]
+    ele = cond_func(y,v,*other_args)
+    return(ele)
 
 
+def fxyvfltre(d):
+    m = d['m']
+    cond_func = d['f']
+    other_args = d['o']
+    x = d['x']
+    y = d['y']
+    v = m[x][y]
+    ele = cond_func(x,y,v,*other_args)
+    return(ele)
 
 
+def fxyofltre(d):
+    m = d['m']
+    cond_func = d['f']
+    other_args = d['o']
+    x = d['x']
+    y = d['y']
+    v = m[x][y]
+    ele = cond_func(x,y,o,*other_args)
+    return(ele)
 
 
+def fxvofltre(d):
+    m = d['m']
+    cond_func = d['f']
+    other_args = d['o']
+    x = d['x']
+    y = d['y']
+    v = m[x][y]
+    ele = cond_func(x,v,o,*other_args)
+    return(ele)
 
 
+def fyvofltre(d):
+    m = d['m']
+    cond_func = d['f']
+    other_args = d['o']
+    x = d['x']
+    y = d['y']
+    v = m[x][y]
+    ele = cond_func(y,v,o,*other_args)
+    return(ele)
 
 
+def xyvofltre(d):
+    m = d['m']
+    cond_func = d['f']
+    other_args = d['o']
+    x = d['x']
+    y = d['y']
+    v = m[x][y]
+    ele = cond_func(x,y,v,*other_args)
+    return(ele)
 
 
+def fxyvofltre(d):
+    m = d['m']
+    cond_func = d['f']
+    other_args = d['o']
+    x = d['x']
+    y = d['y']
+    v = m[x][y]
+    ele = cond_func(x,y,v,o,*other_args)
+    return(ele)
 
 
--- ematmap/filter_udrl.py
+++ ematmap/filter_udrl.py
@@ -1,155 +1,155 @@
 import ematmap.filter_engine as engine
 
-@engine.udlr_wrap
+@engine.udrl_wrap
 def ffltre(d):
     return(engine.ffltre(d))
 
 
-@engine.udlr_wrap
+@engine.udrl_wrap
 def xfltre(d):
     return(engine.xfltre(d))
 
 
-@engine.udlr_wrap
+@engine.udrl_wrap
 def yfltre(d):
     return(engine.yfltre(d))
 
 
-@engine.udlr_wrap
+@engine.udrl_wrap
 def vfltre(d):
     return(engine.vfltre(d))
 
 
-@engine.udlr_wrap
+@engine.udrl_wrap
 def ofltre(d):
     return(engine.ofltre(d))
 
 
-@engine.udlr_wrap
+@engine.udrl_wrap
 def fxfltre(d):
     return(engine.fxfltre(d))
 
 
-@engine.udlr_wrap
+@engine.udrl_wrap
 def fyfltre(d):
     return(engine.fyfltre(d))
 
 
-@engine.udlr_wrap
+@engine.udrl_wrap
 def fvfltre(d):
     return(engine.fvfltre(d))
 
 
-@engine.udlr_wrap
+@engine.udrl_wrap
 def fofltre(d):
     return(engine.fofltre(d))
 
 
-@engine.udlr_wrap
+@engine.udrl_wrap
 def xyfltre(d):
     return(engine.xyfltre(d))
 
 
-@engine.udlr_wrap
+@engine.udrl_wrap
 def xvfltre(d):
     return(engine.xvfltre(d))
 
 
-@engine.udlr_wrap
+@engine.udrl_wrap
 def xofltre(d):
     return(engine.xofltre(d))
 
 
-@engine.udlr_wrap
+@engine.udrl_wrap
 def yvfltre(d):
     return(engine.yvfltre(d))
 
 
-@engine.udlr_wrap
+@engine.udrl_wrap
 def yofltre(d):
     return(engine.yofltre(d))
 
 
-@engine.udlr_wrap
+@engine.udrl_wrap
 def vofltre(d):
     return(engine.vofltre(d))
 
 
-@engine.udlr_wrap
+@engine.udrl_wrap
 def fxyfltre(d):
     return(engine.fxyfltre(d))
 
 
-@engine.udlr_wrap
+@engine.udrl_wrap
 def fxvfltre(d):
     return(engine.fxvfltre(d))
 
 
-@engine.udlr_wrap
+@engine.udrl_wrap
 def fxofltre(d):
     return(engine.fxofltre(d))
 
 
-@engine.udlr_wrap
+@engine.udrl_wrap
 def fyvfltre(d):
     return(engine.fyvfltre(d))
 
 
-@engine.udlr_wrap
+@engine.udrl_wrap
 def fyofltre(d):
     return(engine.fyofltre(d))
 
 
-@engine.udlr_wrap
+@engine.udrl_wrap
 def fvofltre(d):
     return(engine.fvofltre(d))
 
 
-@engine.udlr_wrap
+@engine.udrl_wrap
 def xyvfltre(d):
     return(engine.xyvfltre(d))
 
 
-@engine.udlr_wrap
+@engine.udrl_wrap
 def xyofltre(d):
     return(engine.xyofltre(d))
 
 
-@engine.udlr_wrap
+@engine.udrl_wrap
 def xvofltre(d):
     return(engine.xvofltre(d))
 
 
-@engine.udlr_wrap
+@engine.udrl_wrap
 def yvofltre(d):
     return(engine.yvofltre(d))
 
 
-@engine.udlr_wrap
+@engine.udrl_wrap
 def fxyvfltre(d):
     return(engine.fxyvfltre(d))
 
 
-@engine.udlr_wrap
+@engine.udrl_wrap
 def fxyofltre(d):
     return(engine.fxyofltre(d))
 
 
-@engine.udlr_wrap
+@engine.udrl_wrap
 def fxvofltre(d):
     return(engine.fxvofltre(d))
 
 
-@engine.udlr_wrap
+@engine.udrl_wrap
 def fyvofltre(d):
     return(engine.fyvofltre(d))
 
 
-@engine.udlr_wrap
+@engine.udrl_wrap
 def xyvofltre(d):
     return(engine.xyvofltre(d))
 
 
-@engine.udlr_wrap
+@engine.udrl_wrap
 def fxyvofltre(d):
     return(engine.fxyvofltre(d))
